7929d6356fd49ba31b1004f6639c5ddea8b91db9
Volker Stolz <vsto@hvl.no>
2024-11-25 14:47:02 +0100

    mark

--- soundness-checker/src/Maude.hs
+++ soundness-checker/src/Maude.hs
@@ -1,25 +1,42 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Maude where
 
 import Control.Lens
+import qualified Data.Map as M
 import Types
 
-addFeature :: FeatureModel -> (FeatureID, Name, GroupID, FeatureType) -> FeatureModel
-addFeature old (newFid, newName, targetGroup, fType)
- | notExists newFid (_rootFeature old) && isUniqueName newName (_rootFeature old) = FeatureModel { _rootFeature = addFeatureToGroup (_rootFeature old) targetGroup newFid }
+type FT = M.Map FeatureID Feature
+data FM = FM FeatureID FT
+  deriving (Show, Eq)
+
+makeFieldsNoPrefix ''FM
+
+addFeature :: FM -> (FeatureID, Name, GroupID, FeatureType) -> FM
+addFeature (FM rfid ft) (newFid, newName, targetGroup, fType)
+ | notExists newFid ft && isUniqueName newName (((map snd) . M.toList) ft) = FM rfid (addFeatureToGroup ft targetGroup newFid)
  | otherwise   = error "Not allowed"
 
-notExists :: FeatureID -> Feature -> Bool
-notExists fid f
- | _featureID f == fid = False
- | otherwise = all (\g -> all (notExists fid) (_childFeatures g)) (_childGroups f)
+notExists :: FeatureID -> FT -> Bool
+notExists fid ft = M.notMember fid ft
 
-isUniqueName :: Name -> Feature -> Bool
-isUniqueName n f
- | _name f == n = False
- | otherwise = all (\g -> all (isUniqueName n) (_childFeatures g)) (_childGroups f)
+mkGroupMap :: [Group] -> M.Map GroupID Group
+mkGroupMap = M.fromList . (map (\x -> (_groupID x,x)))
 
-addFeatureToGroup :: Feature -> GroupID -> FeatureID -> Feature
-addFeatureToGroup f targetGroup newFid = updateF f (_childGroups) (map (addFeatureToGroup_g targetGroup newFid) (_childGroups f))
+mkFeatureMap :: [Feature] -> FT
+mkFeatureMap = M.fromList . (map (\x -> (_featureID x,x)))
+
+isUniqueName :: Name -> [Feature] -> Bool
+isUniqueName n ft = all (\f -> all (\g -> isUniqueName n (_childFeatures g))
+                                   (_childGroups f)) ft
+
+addFeatureToGroup :: FT -> GroupID -> FeatureID -> FT
+addFeatureToGroup ft targetGroup newFid = M.map (\f -> updateF f (_childGroups) (map (addFeatureToGroup_g targetGroup newFid) (_childGroups f))) ft
 
 addFeatureToGroup_g :: GroupID -> FeatureID -> Group -> Group
 addFeatureToGroup_g gid fid g
