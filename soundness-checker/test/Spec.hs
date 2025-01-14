3fd520c5087326fbc9325a81927d312a2b1da46c
Volker Stolz <vsto@hvl.no>
2025-01-14 10:36:58 +0100

    CI

--- soundness-checker/test/Spec.hs
+++ soundness-checker/test/Spec.hs
@@ -1,2 +1,13 @@
+import Test.Hspec
+import Test.Hspec.Contrib.HUnit (fromHUnitTest)
+
+import Experiments
+import TestRenameIssue
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Running some HUnit tests" $ do
+    fromHUnitTest tests_debugging
+    fromHUnitTest tests_equal
+    fromHUnitTest tests_smallFlatProblem
+    fromHUnitTest tests_equal_all_plans
