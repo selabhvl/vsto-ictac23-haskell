bb0c944afc211240c089f2d3a6283ad063bb8a61
Volker Stolz <vsto@hvl.no>
2025-01-17 14:35:04 +0100

    tests

--- soundness-checker/test/Spec.hs
+++ soundness-checker/test/Spec.hs
@@ -1,13 +1,18 @@
 import Test.Hspec
 import Test.Hspec.Contrib.HUnit (fromHUnitTest)
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
 
 import Experiments
 import TestRenameIssue
 
 main :: IO ()
 main = hspec $ do
-  describe "Running some HUnit tests" $ do
+  describe "Running some HUnit tests:" $ do
     fromHUnitTest tests_debugging
     -- fromHUnitTest tests_equal
     fromHUnitTest tests_smallFlatProblem
     fromHUnitTest tests_equal_all_plans
+  describe "...and some quickchecking:" $ do
+    mapM_ (\(n,p) -> prop ("MoveFeature in " ++ n) $ prop_moveFeature p) allPlans
+    mapM_ (\(n,p) -> prop ("MoveGroup in " ++ n) $ prop_moveGroup p) allPlans
